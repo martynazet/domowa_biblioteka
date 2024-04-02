bcc62de353252fa16ff4e291aadf08b94a363e61
Martyna Zawór <mart.zaw.95@gmail.com>
2024-04-03 00:36:50 +0200

    Add models file

---
+++ models_biblioteka.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*
+import json
+
+class Library:
+    def __init__(self):
+        file_path = "C:\\Users\\User\\Desktop\\Kodilla\\Python\Flask\\library.json"
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                self.library = json.load(f)
+        except FileNotFoundError:
+            self.library = []
+
+    def all(self):
+        return self.library
+    
+    def get(self, id):
+        book = [book for book in self.all() if book["id"] == id]
+        if book:
+            return book[0]
+        return []
+    
+    def create(self, data):
+        self.library.append(data)
+        self.save_all()
+
+    def save_all(self):
+        file_path = "C:\\Users\\User\\Desktop\\Kodilla\\Python\\Flask\\library.json"
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(self.library, f)
+
+    def update(self, id, data):
+        book = self.get(id)
+        if book:
+            index = self.library.index(book)
+            self.library[index] = data
+            self.save_all()
+            return True
+        return False
+
+    def delete(self, id):
+        book = self.get(id)
+        if book:
+            self.library.remove(book)
+            self.save_all()
+            return book["title"]
+        return False
+
+library = Library()
